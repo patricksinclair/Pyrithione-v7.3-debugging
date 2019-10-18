4b6477376e61d15dd548caa6030349aa4cbe4ef2
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2019-10-18 16:23:43 +0100

    more taus

--- src/PyrithioneMain.java
+++ src/PyrithioneMain.java
@@ -28,10 +28,9 @@ public class PyrithioneMain {
         long startTime = System.currentTimeMillis();
 
         double[] tau_vals = {0.005, 0.01, 0.02, 0.05, 0.1};
-        double[] tau_vals2 = {0.005, 0.01, 0.1};
         int nreps = 32;
 
-        for(int i = 0; i < tau_vals2.length; i++){
+        for(int i = 0; i < tau_vals.length; i++){
             BioSystem.debugReplications(tau_vals[i], nreps);
             BioSystem.debugDeaths(tau_vals[i], nreps);
             BioSystem.debugImmigration(tau_vals[i], nreps);
